6e2f1ded6b0034dff87fb71e8823e40f25c71563
Hudson Guedes <hudsonmende@hotmail.com>
2021-05-14 16:56:07 -0300

    Ajustando os controllers e os models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,12 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @events = Event.all.map do |item|
+      {
+        id: item.id,
+        title: item.description,
+        start: item.initial_date.strftime('%Y-%m-%d %H:%M:%S'),
+        end: item.final_date.strftime('%Y-%m-%d %H:%M:%S')
+      }
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class Event < ApplicationRecord
+  belongs_to :user
+  validates :description, :initial_date, :final_date, presence: true
+  validates :initial_date, :final_date, uniqueness: true
+  validate :bussiness_hour
+  validate :bussiness_date
+
+  def bussiness_hour
+    if initial_date.nil? || initial_date.hour < 6 || initial_date.hour > 23
+      errors.add(:base, 'invalid hour')
+    end
 end
